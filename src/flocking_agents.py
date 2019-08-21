93f60153168266d52178972adbb26978c0e097ca
Raphael Loyola <raphael.braccialli@gmail.com>
2019-08-21 12:04:01 -0300

    media movel

--- src/flocking_agents.py
+++ src/flocking_agents.py
@@ -4,7 +4,6 @@ import sys
 import rospy
 import math
 import numpy as np
-from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from turtlesim.msg import Pose as TurtlePose
@@ -16,6 +15,7 @@ consensus_state = np.zeros([8, 1])
 #                     0   1   2   3       4       5		  6		  7
 # consensus_state = [v0, v1, v2, v3, theta0, theta1, theta2, theta3]
 
+ma_size = 5 # moving average size
 
 #edit add offset
 
@@ -25,24 +25,19 @@ print('global variables initialized')
 # TO DO
 # adicionar offsets de posicao
 
-def update_yawl(msg):
-	global consensus_state
-
-	consensus_state[4,0] = msg.data
-	# consensus_state[4, 0] = euler_from_quaternion(quaternion)[2] # orientation n rads
 
 def update_pose0(msg):
 	#print('update_pose1')
 	global consensus_state
 
-	# quaternion = (
-	#     msg.pose.pose.orientation.x,
-	#     msg.pose.pose.orientation.y,
-	#     msg.pose.pose.orientation.z,
-	#     msg.pose.pose.orientation.w)
+	quaternion = (
+	    msg.pose.pose.orientation.x,
+	    msg.pose.pose.orientation.y,
+	    msg.pose.pose.orientation.z,
+	    msg.pose.pose.orientation.w)
 
 	consensus_state[0, 0] = msg.twist.twist.linear.x # linear speed
-	# consensus_state[4, 0] = euler_from_quaternion(quaternion)[2] # orientation n rads
+	consensus_state[4, 0] = euler_from_quaternion(quaternion)[2] # orientation n rads
 
 def update_pose1(msg):
 	#print('update_pose1')
@@ -54,8 +49,8 @@ def update_pose1(msg):
 	    msg.pose.pose.orientation.z,
 	    msg.pose.pose.orientation.w)
 
-	consensus_state[1, 0] = msg.twist.twist.linear.x # linear speed
-	consensus_state[5, 0] = euler_from_quaternion(quaternion)[2] # orientation n rads
+	consensus_state[1, 0] = consensus_state[1, 0]*(ma_size-1)/ma_size + msg.twist.twist.linear.x/ma_size # linear speed
+	consensus_state[5, 0] = consensus_state[5, 0]*(ma_size-1)/ma_size + euler_from_quaternion(quaternion)[2]/ma_size # orientation n rads
 
 
 
@@ -69,13 +64,13 @@ def update_pose2(msg):
 	    msg.pose.pose.orientation.z,
 	    msg.pose.pose.orientation.w)
 
-	consensus_state[2, 0] = msg.twist.twist.linear.x # linear speed
-	consensus_state[6, 0] = euler_from_quaternion(quaternion)[2] # orientation n rads
+	consensus_state[2, 0] = consensus_state[2, 0]*(ma_size-1)/ma_size + msg.twist.twist.linear.x/ma_size # linear speed
+	consensus_state[6, 0] = consensus_state[6, 0]*(ma_size-1)/ma_size + euler_from_quaternion(quaternion)[2]/ma_size # orientation n rads
 
 	# print(consensus_state)
 
 def update_pose3(msg):
-	# print('update_pose3')
+	print('update_pose3')
 	global consensus_state
 
 	quaternion = (
@@ -84,8 +79,8 @@ def update_pose3(msg):
 	    msg.pose.pose.orientation.z,
 	    msg.pose.pose.orientation.w)
 
-	consensus_state[3, 0] = msg.twist.twist.linear.x # linear speed
-	consensus_state[7, 0] = euler_from_quaternion(quaternion)[2] # orientation n rads
+	consensus_state[3, 0] = consensus_state[3, 0]*(ma_size-1)/ma_size + msg.twist.twist.linear.x/ma_size # linear speed
+	consensus_state[7, 0] = consensus_state[7, 0]*(ma_size-1)/ma_size + euler_from_quaternion(quaternion)[2]/ma_size # orientation n rads
 
 
 
@@ -103,7 +98,6 @@ if __name__ == '__main__':
 	print(robot_name[this_robot] + " is on!")
 	# starts subscribers and publishers
 	rospy.Subscriber("/leader/pose", Odometry, update_pose0)
-	rospy.Subscriber("/leader/yaw", Float32, update_yawl)
 	rospy.Subscriber("/aramis/pose", Odometry, update_pose1)
 	rospy.Subscriber("/athos/pose", Odometry, update_pose2)
 	rospy.Subscriber("/porthos/pose", Odometry, update_pose3)
@@ -113,8 +107,8 @@ if __name__ == '__main__':
 
 
 	# control variables 
-	k = 0.7
-	l = 0.05
+	k = 1
+	l = 1
 
 	# calculates laplacian
 	A = np.array([[0, 0, 0, 0],
@@ -123,55 +117,54 @@ if __name__ == '__main__':
 				  [1, 1, 1, 0]])
 
 	cmd_vel = Twist()
-	delta_t = 1
+	delta_t = 0.1
 	rate = rospy.Rate(1/delta_t)
 
 	this_robot += 1
 
-	teste = 0
 	while not rospy.is_shutdown():
+				
+
+
 				u1_sum = 0
 				for j in range(n):
-					v_error = consensus_state[this_robot, 0] - consensus_state[j, 0]
-					u1_sum += A[this_robot, j] * v_error
-				
-				u1 = -(k * u1_sum) - (l * np.sign(u1_sum))
-				print(u1, -(k * u1_sum), - (l * np.sign(u1_sum)))
+					if(j != this_robot):
+						v_error = consensus_state[this_robot, 0] - consensus_state[j, 0]
+						u1_sum += A[this_robot, j]*v_error
+
+				u1 = -k * u1_sum - l * np.sign(u1_sum)
 
 
 
 				u2_sum = 0
 				for j in range(n):
-					theta_error = consensus_state[n + this_robot, 0] - consensus_state[n + j, 0]
-					u2_sum += A[this_robot, j] * theta_error
+					if(j != this_robot):
+						theta_error = consensus_state[n + this_robot, 0] - consensus_state[n + j, 0]
+						u2_sum += A[this_robot, j]*theta_error
 
-				u2 = -(k * u2_sum) - (l * np.sign(u2_sum))
+				u2 = -k * u2_sum - l * np.sign(u2_sum)
 
 
 
 				v = consensus_state[this_robot, 0]
 
-				print(v)
 				v = v + u1 * delta_t
 				w = u2
-				print(v)
 
-				# print(w)
+				#print(v)
+				#print(w)
 
-				# if v > 0.1:
-				# 	v = 0.1
-				# elif v < -0.1:
-				# 	v = -0.1
+				if v > 0.1:
+					v = 0.1
+				elif v < -0.1:
+					v = -0.1
 
-				# if w > 0.1:
-				# 	w = 0.1
-				# elif w < -0.1:
-				# 	w = -0.1
+				if w > 0.1:
+					w = 0.1
+				elif w < -0.1:
+					w = -0.1
 
 				cmd_vel.linear.x = v
-				# if(this_robot == 2):
-				# 	cmd_vel.linear.x = 1 - teste*0.01
-				# 	teste+=1
 				cmd_vel.angular.z = w
 
 				pub.publish(cmd_vel)
